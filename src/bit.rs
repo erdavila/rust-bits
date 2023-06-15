8b43eca1ea766681955f30e635e80b5d5534e781
Eduardo R. D'Avila <erdavila@gmail.com>
2023-06-15 17:15:48 -0300

    Bit: implement modify() method

--- src/bit.rs
+++ src/bit.rs
@@ -95,8 +95,50 @@ impl Bit {
         }
     }
 
+    /// Allows retrieving and setting the bit value in a single operation.
+    ///
+    /// The passed closure receives the current value and must return the new value.
+    ///
+    /// # Example
+    ///
+    /// TODO
+    #[inline]
     pub fn modify<F: FnOnce(BitValue) -> BitValue>(&mut self, f: F) {
-        todo!()
+        let repr: RefRepr = unsafe { std::mem::transmute(self) };
+        let components = repr.decode();
+        debug_assert_eq!(components.metadata.bit_count, 1);
+
+        fn modify<U: BitsPrimitive, F: FnOnce(BitValue) -> BitValue>(
+            ptr: NonNull<()>,
+            offset: usize,
+            f: F,
+        ) {
+            let mut accessor = BitAccessor::new(ptr.cast::<U>(), offset);
+            let previous_value = accessor.get();
+            let new_value = f(previous_value);
+            accessor.set(new_value);
+        }
+
+        match components.metadata.underlying_primitive {
+            BitsPrimitiveDiscriminant::Usize => {
+                modify::<usize, F>(components.ptr, components.metadata.offset, f)
+            }
+            BitsPrimitiveDiscriminant::U8 => {
+                modify::<u8, F>(components.ptr, components.metadata.offset, f)
+            }
+            BitsPrimitiveDiscriminant::U16 => {
+                modify::<u16, F>(components.ptr, components.metadata.offset, f)
+            }
+            BitsPrimitiveDiscriminant::U32 => {
+                modify::<u32, F>(components.ptr, components.metadata.offset, f)
+            }
+            BitsPrimitiveDiscriminant::U64 => {
+                modify::<u64, F>(components.ptr, components.metadata.offset, f)
+            }
+            BitsPrimitiveDiscriminant::U128 => {
+                modify::<u128, F>(components.ptr, components.metadata.offset, f)
+            }
+        }
     }
 }
 
@@ -132,6 +174,8 @@ impl<P: BitsPrimitive> BitAccessor<P> {
 
 #[cfg(test)]
 mod tests {
+    use std::convert::identity;
+    use std::ops::Not;
     use std::ptr::NonNull;
 
     use crate::refrepr::TypedRefComponents;
@@ -181,5 +225,27 @@ mod tests {
         assert_eq!(new_mut(2).write(Zero), Zero);
         assert_eq!(new_mut(3).write(One), Zero);
         assert_eq!(under, 0b10011010);
+    }
+
+    #[test]
+    fn modify() {
+        let under = 0b10010011u8;
+
+        let new_mut = |bit_index| -> &mut Bit {
+            let components = TypedRefComponents {
+                ptr: NonNull::from(&under),
+                offset: bit_index,
+                bit_count: 1,
+            };
+            let repr = components.encode();
+            unsafe { std::mem::transmute(repr) }
+        };
+
+        new_mut(4).modify(Not::not);
+        new_mut(5).modify(Not::not);
+        new_mut(6).modify(identity);
+        new_mut(7).modify(identity);
+
+        assert_eq!(under, 0b10100011);
     }
 }
