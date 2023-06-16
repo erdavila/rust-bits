7c72020127d58906a3a2bc0e330bc9c23424eae3
Eduardo R. D'Avila <erdavila@gmail.com>
2023-06-16 19:21:35 -0300

    BitStr: implement get() and get_ref() methods

--- src/bitstr.rs
+++ src/bitstr.rs
@@ -1,7 +1,7 @@
 use std::ptr::NonNull;
 
 use crate::refrepr::{RefRepr, TypedRefComponents};
-use crate::BitsPrimitive;
+use crate::{Bit, BitValue, BitsPrimitive};
 
 /// A reference to a fixed-length sequence of bits anywhere in [underlying memory].
 ///
@@ -46,6 +46,32 @@ impl BitStr {
     pub fn is_empty(&self) -> bool {
         self.len() == 0
     }
+
+    /// Returns the value of a bit.
+    ///
+    /// `None` is returned if the index is out of bounds.
+    #[inline]
+    pub fn get(&self, index: usize) -> Option<BitValue> {
+        self.get_ref(index).map(|bit_ref| bit_ref.read())
+    }
+
+    /// Returns the reference of a bit.
+    ///
+    /// `None` is returned if the index is out of bounds.
+    #[inline]
+    pub fn get_ref(&self, index: usize) -> Option<&Bit> {
+        let repr: RefRepr = unsafe { std::mem::transmute(self) };
+        let mut components = repr.decode();
+        if index < components.metadata.bit_count {
+            components.metadata.offset += index;
+            components.metadata.bit_count = 1;
+            let repr = components.encode();
+            let bit_ref = unsafe { std::mem::transmute(repr) };
+            Some(bit_ref)
+        } else {
+            None
+        }
+    }
 }
 
 #[cfg(test)]
@@ -53,7 +79,8 @@ mod tests {
     use std::ptr::NonNull;
 
     use crate::refrepr::RefRepr;
-    use crate::{BitStr, BitsPrimitive};
+    use crate::BitValue::{One, Zero};
+    use crate::{Bit, BitStr, BitsPrimitive};
 
     #[test]
     fn new_ref() {
@@ -83,5 +110,62 @@ mod tests {
         assert_new_ref_with_type!(u32);
         assert_new_ref_with_type!(u64);
         assert_new_ref_with_type!(u128);
+    }
+
+    #[test]
+    fn get() {
+        let memory: [u8; 2] = [0b10010011, 0b01101100];
+        let bit_str = BitStr::new_ref(memory.as_ref());
+
+        assert_eq!(bit_str.get(0), Some(One));
+        assert_eq!(bit_str.get(1), Some(One));
+        assert_eq!(bit_str.get(2), Some(Zero));
+        assert_eq!(bit_str.get(3), Some(Zero));
+        assert_eq!(bit_str.get(4), Some(One));
+        assert_eq!(bit_str.get(5), Some(Zero));
+        assert_eq!(bit_str.get(6), Some(Zero));
+        assert_eq!(bit_str.get(7), Some(One));
+
+        assert_eq!(bit_str.get(8), Some(Zero));
+        assert_eq!(bit_str.get(9), Some(Zero));
+        assert_eq!(bit_str.get(10), Some(One));
+        assert_eq!(bit_str.get(11), Some(One));
+        assert_eq!(bit_str.get(12), Some(Zero));
+        assert_eq!(bit_str.get(13), Some(One));
+        assert_eq!(bit_str.get(14), Some(One));
+        assert_eq!(bit_str.get(15), Some(Zero));
+
+        assert_eq!(bit_str.get(16), None);
+        assert_eq!(bit_str.get(17), None);
+        assert_eq!(bit_str.get(18), None);
+    }
+
+    #[test]
+    fn get_ref() {
+        let memory: [u8; 2] = [0b10010011, 0b01101100];
+        let bit_str = BitStr::new_ref(memory.as_ref());
+
+        let bit_ref: Option<&Bit> = bit_str.get_ref(0);
+        assert_eq!(bit_ref.unwrap().read(), One);
+        assert_eq!(bit_str.get_ref(1).unwrap().read(), One);
+        assert_eq!(bit_str.get_ref(2).unwrap().read(), Zero);
+        assert_eq!(bit_str.get_ref(3).unwrap().read(), Zero);
+        assert_eq!(bit_str.get_ref(4).unwrap().read(), One);
+        assert_eq!(bit_str.get_ref(5).unwrap().read(), Zero);
+        assert_eq!(bit_str.get_ref(6).unwrap().read(), Zero);
+        assert_eq!(bit_str.get_ref(7).unwrap().read(), One);
+
+        assert_eq!(bit_str.get_ref(8).unwrap().read(), Zero);
+        assert_eq!(bit_str.get_ref(9).unwrap().read(), Zero);
+        assert_eq!(bit_str.get_ref(10).unwrap().read(), One);
+        assert_eq!(bit_str.get_ref(11).unwrap().read(), One);
+        assert_eq!(bit_str.get_ref(12).unwrap().read(), Zero);
+        assert_eq!(bit_str.get_ref(13).unwrap().read(), One);
+        assert_eq!(bit_str.get_ref(14).unwrap().read(), One);
+        assert_eq!(bit_str.get_ref(15).unwrap().read(), Zero);
+
+        assert_eq!(bit_str.get_ref(16), None);
+        assert_eq!(bit_str.get_ref(17), None);
+        assert_eq!(bit_str.get_ref(18), None);
     }
 }
