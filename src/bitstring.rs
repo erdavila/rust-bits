c62e7f63ed797efc422abb0e6db17bc3cd0ac126
Eduardo R. D'Avila <erdavila@gmail.com>
2023-06-30 11:06:01 -0300

    BitString: implement as_bit_str{,_mut}() methods and Deref{,Mut} traits

--- src/bitstring.rs
+++ src/bitstring.rs
@@ -1,6 +1,10 @@
+use std::ops::{Deref, DerefMut};
+use std::ptr::NonNull;
+
 use linear_deque::LinearDeque;
 
-use crate::BitsPrimitive;
+use crate::refrepr::{RefRepr, TypedRefComponents};
+use crate::{BitStr, BitsPrimitive};
 
 #[derive(Clone, Debug)]
 pub struct BitString<U: BitsPrimitive = usize> {
@@ -35,6 +39,45 @@ impl<U: BitsPrimitive> BitString<U> {
     pub fn is_empty(&self) -> bool {
         self.len() == 0
     }
+
+    #[inline]
+    pub fn as_bit_str(&self) -> &BitStr {
+        let repr = self.as_repr();
+        unsafe { std::mem::transmute(repr) }
+    }
+
+    #[inline]
+    pub fn as_bit_str_mut(&mut self) -> &mut BitStr {
+        let repr = self.as_repr();
+        unsafe { std::mem::transmute(repr) }
+    }
+
+    #[inline]
+    fn as_repr(&self) -> RefRepr {
+        let components = TypedRefComponents {
+            ptr: NonNull::from(self.buffer.deref()).cast::<U>(),
+            offset: self.offset,
+            bit_count: self.bit_count,
+        };
+
+        components.encode()
+    }
+}
+
+impl<U: BitsPrimitive> Deref for BitString<U> {
+    type Target = BitStr;
+
+    #[inline]
+    fn deref(&self) -> &Self::Target {
+        self.as_bit_str()
+    }
+}
+
+impl<U: BitsPrimitive> DerefMut for BitString<U> {
+    #[inline]
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        self.as_bit_str_mut()
+    }
 }
 
 impl<U: BitsPrimitive> Default for BitString<U> {
@@ -46,12 +89,22 @@ impl<U: BitsPrimitive> Default for BitString<U> {
 
 #[cfg(test)]
 mod tests {
-    use crate::BitString;
+    use crate::{BitStr, BitString};
 
     #[test]
     fn new() {
         let string: BitString = BitString::new();
 
         assert_eq!(string.len(), 0);
+    }
+
+    #[test]
+    fn deref() {
+        let string: BitString = BitString::new();
+
+        let str: &BitStr = &string;
+
+        assert_eq!(str.len(), 0);
+        assert!(str.is_empty());
     }
 }
